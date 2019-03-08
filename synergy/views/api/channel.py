f95b124c9e92f706c0797fdf1b90980206565b02
Sarah Johnson <sajohnson2468@gmail.com>
2019-03-08 15:39:44 -0800

    Added channel and device handlers

--- synergy/views/api/channel.py
+++ synergy/views/api/channel.py
@@ -1,9 +1,37 @@
 from pyramid.view import view_config
 from pyramid.response import Response
+from ...database.devices.channels import get_channels, update_channel, get_all_channels
 import json
 
+def switch_action(type):
+    actions = {
+        'get': get_channels,
+        'update': update_channel,
+        'all' : get_all_channels,
+    }
+    return actions.get(type, defaultResponse)
+
 @view_config(route_name='channel', renderer='json', request_method='POST')
-def login_handler(request):
-    response = Response(content_type='application/json')
-    response.json_body = json.dumps(request.json_body)
-    return response
+def channel_handler(request):
+    action = request.json_body
+
+    if 'type' in action and 'payload' in action:
+        actionType = action['type']
+        callback = switch_action(actionType)
+
+        try:
+            response = callback(action['payload'])
+            if 'error' not in response:
+                return responseSuccess(response, actionType)
+            else:
+                return responseError(response['errors'], actionType, 400)
+
+        except Exception as error:
+            response = reportError(
+                'An error occured handling the group request', error)
+            return responseError(response, actionType, 500)
+
+    response = reportError(
+        'The group request was not formatted correctly', None)
+    return responseError(response, None, 400)
+
--- synergy/views/api/device.py
+++ synergy/views/api/device.py
@@ -1,9 +1,36 @@
 from pyramid.view import view_config
 from pyramid.response import Response
+from ...database.devices.devices import get_device, update_device, get_all_devices
 import json
 
+def switch_action(type):
+    actions = {
+        'get': get_device,
+        'update': update_device,
+        'all' : get_all_devices,
+    }
+    return actions.get(type, defaultResponse)
+
 @view_config(route_name='device', renderer='json', request_method='POST')
-def login_handler(request):
-    response = Response(content_type='application/json')
-    response.json_body = json.dumps(request.json_body)
-    return response
+def device_handler(request):
+    action = request.json_body
+
+    if 'type' in action and 'payload' in action:
+        actionType = action['type']
+        callback = switch_action(actionType)
+
+        try:
+            response = callback(action['payload'])
+            if 'error' not in response:
+                return responseSuccess(response, actionType)
+            else:
+                return responseError(response['errors'], actionType, 400)
+
+        except Exception as error:
+            response = reportError(
+                'An error occured handling the group request', error)
+            return responseError(response, actionType, 500)
+
+    response = reportError(
+        'The group request was not formatted correctly', None)
+    return responseError(response, None, 400)
